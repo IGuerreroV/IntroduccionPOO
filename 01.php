cdb4ac0ccb6e89f434fe86035176bf4c5faec3e3
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-09 16:21:36 -0500

    Métodos en classes

--- 01.php
+++ 01.php
@@ -8,9 +8,14 @@ class Producto {
     public function __construct(public string $nombre, public int $precio, public bool $disponible)
     {
     }
+
+    public function mostrarProducto() {
+        echo "El producto es: " . $this->nombre . " y su precio es de: " . $this->precio;
+    }
 }
 
 $producto = new Producto('Tablet', 200, true);
+$producto->mostrarProducto();
 
 
 echo '<pre>';
@@ -18,6 +23,7 @@ var_dump($producto);
 echo '</pre>';
 
 $producto2 = new Producto('Monitor Curvo', 300, true);
+$producto2->mostrarProducto();
 
 echo '<pre>';
 var_dump($producto2);
