635f93190fc75af2b8d0d0a319f115040686cdbf
Ivan Guerrero <ivangvprogrammer@gmail.com>
2024-08-09 22:02:28 -0500

    Namespaces

--- 08.php
+++ 08.php
@@ -1,8 +1,15 @@
 <?php include 'includes/header.php';
 
+// require 'clases/Clientes.php';
+// require 'clases/Detalles.php';
+
+use App\Clientes;
+use App\Detalles;
+
 // Incluir las otras clases
 function mi_autoload($clase) {
-    require __DIR__ . '/clases/' . $clase . '.php';
+    $partes = explode('\\', $clase);
+    require __DIR__ . '/clases/' . $partes[1] . '.php';
 }
 
 spl_autoload_register('mi_autoload');
